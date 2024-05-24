86568ddabb0260d830ba697bbf55a82b658af043
Nhatthach2703 <61878027+Nhatthach2703@users.noreply.github.com>
2024-05-24 16:45:24 +0700

    First commit

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -2,6 +2,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,6 +29,30 @@ class CalculatorTest {
     @Test
     void add() {
         assertEquals(4, cal.Add(2,2));
+    }
+
+    @ParameterizedTest
+    @DisplayName("Test add parameter")
+    @CsvSource({
+            "5, 2, 3",
+            "7, 3, 4",
+            "11, 10, 1",
+            "10, 5, 5"
+    })
+    void testAdd(String expectedStr, String aStr, String bStr) {
+        int a = Integer.parseInt(aStr);
+        int b = Integer.parseInt(bStr);
+        int expected = Integer.parseInt(expectedStr);
+        assertEquals(expected, cal.Add(a, b));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/test.csv", numLinesToSkip = 1)
+    void add3(String aStr, String bStr, String expectedStr) {
+        int a = Integer.parseInt(aStr);
+        int b = Integer.parseInt(bStr);
+        int expected = Integer.parseInt(expectedStr);
+        assertEquals(expected, cal.Add(a, b));
     }
 
 
@@ -61,6 +88,21 @@ class CalculatorTest {
     void testDivide2() {
         Exception exception = assertThrows(ArithmeticException.class, () -> cal.Divide(3, 0));
         assertEquals("Divide by zero", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/test2.csv", numLinesToSkip = 1)
+    @DisplayName("Divide 2 number - 3")
+    void testDivide3(String aStr, String bStr, String expectedStr) {
+        int a = Integer.parseInt(aStr);
+        int b = Integer.parseInt(bStr);
+        if ("Divide by zero".equals(expectedStr)) {
+            Exception exception = assertThrows(ArithmeticException.class, () -> cal.Divide(a, b));
+            assertEquals(expectedStr, exception.getMessage());
+        } else {
+            double expected = Double.parseDouble(expectedStr);
+            assertEquals(expected, cal.Divide(a, b));
+        }
     }
 
 }
